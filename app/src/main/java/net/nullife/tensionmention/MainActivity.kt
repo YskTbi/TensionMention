049225723bd3517aeec55f44715404e1406c0cce
YskTbi <saku.y04@gmail.com>
2018-12-14 13:57:45 +0900

    first commit

---
+++ app/src/main/java/net/nullife/tensionmention/MainActivity.kt
@@ -0,0 +1,34 @@
+package net.nullife.tensionmention
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                message.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_calender -> {
+                message.setText(R.string.title_calendar)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_account -> {
+                message.setText(R.string.title_account)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+}
